d9cc2b194f016156867ef2e0c3fcf21f50d7f67d
Petros Kyriakou <9pitops@gmail.com>
2019-12-24 17:48:39 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+
+const fetch = require('node-fetch')
+const Slack = require('slack-node')
+const util = require('util')
+
+const slackWebhookSecret = process.env.SLACK_WEBHOOK_SECRET
+
+const slack = new Slack()
+slack.setWebhook(`https://hooks.slack.com/services/${slackWebhookSecret}`)
+const slackWebhook = util.promisify(slack.webhook)
+
+const URLs = [
+  {
+    name: 'Bookis Landing page',
+    url: 'https://bookis.io'
+  },
+  {
+    name: 'Bookis app',
+    url: 'https://app.bookis.io'
+  }
+]
+
+const ping = async url => {
+  const res = await fetch(url)
+}
+
+const postToSlack = async message => {
+  await slackWebhook({
+    channel: 'services-status',
+    username: 'Kokoras',
+    text: message
+  })
+}
+
+const getDateTimeString = () => {
+  const event = new Date()
+
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  }
+
+  return event.toLocaleDateString('en-gb', options)
+}
+
+const main = async () => {
+  postToSlack(`============================== (${getDateTimeString()})`)
+  for (let i = 0; i < URLs.length; i++) {
+    const { name, url } = URLs[i]
+    try {
+      await ping(url)
+
+      await postToSlack(`[*${name}*] ✅`)
+    } catch (err) {
+      console.error(err)
+      await postToSlack(
+        `[*${name}*] UNREACHABLE!!! 🚨🚨🚨 \n Error: ${err.message}`
+      )
+    }
+  }
+  postToSlack(`==============================`)
+}
+
+main()
